b4af8647c6f4a9b12ff5d716964b03cd02f7b3a3
shindeAishvarya <shindeaishvarya75@gmail.com>
2023-01-06 15:40:44 +0530

    second commit

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -120,8 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 LOGIN_REDIRECT_URL = '/profile/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
